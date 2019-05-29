f095af3f2deb21ad00021dab0ebc7800181cc26d
david <david.stankov@haemimont.com>
2019-05-29 14:22:51 +0300

    Tree codes Algoritams example  https://www.geeksforgeeks.org/quad-tree/  https://algs4.cs.princeton.edu/92search/QuadTree.java.html

---
+++ excercise/ExcerciseDataStructure.py
@@ -0,0 +1,70 @@
+"""
+author : Daodeiv
+"""
+
+
+class Node:
+
+    def __init__(self, key_x=0, key_y=0, value=None, range=[4, 4]):
+        self.x = key_x  # x coordinates
+        self.y = key_y  # y cordinates
+        self.value = value
+        """
+         Quadrants
+        |11|12' 
+        |21|22|
+        _
+        """
+        self.root = None
+        self.Q_11 = None  # Quadrant 11
+        self.Q_12 = None  # Quadrant 12
+        self.Q_21 = None  # Quadrant 21
+        self.Q_22 = None  # Quadrant 22
+
+    def __add__(self, node, key_x, key_y, value):
+        if key_x <= node.x and key_y >= node.y:
+            if node.Q_11 is None:
+               node.Q_11 = Node(key_x, key_y, value)
+               return
+            self.__add__(node.Q_11, key_x, key_y, value)
+        elif key_x >= node.x and key_y >= node.y:
+            if node.Q_12 is None:
+                node.Q_12 = Node(key_x, key_y, value)
+                return
+            self.__add__(node.Q_12, key_x, key_y, value)
+        elif key_x <= node.x and key_y <= node.y:
+            if node.Q_21 is None:
+                node.Q_21 = Node(key_x, key_y, value)
+                return
+            self.__add__(node.Q_21, key_x, key_y, value)
+        elif key_x >= node.x and key_y <= node.y:
+            if node.Q_22 is None:
+                node.Q_22 = Node(key_x, key_y, value)
+                return
+            node.__add__(node.Q_22, key_x, key_y, value)
+
+
+
+
+class Tree:
+    def __init__(self):
+        self.root = None;
+
+    def add_element(self, key_x, key_y, value):
+        if self.root is None:
+            self.root = Node(key_x, key_y, value)
+
+        else:
+            self.root.__add__(self.root, key_x, key_y, value)
+
+
+
+
+t = Tree()
+
+t.add_element(key_x=0,key_y=0,value="First 0 0 ")
+t.add_element(1,1,"First 1 1 ")
+t.add_element(2,3,"First 2 3 ")
+t.add_element(-1,2,"First -1 2  ")
+t.add_element(-1,1,"First - 1 1 ")
+print('Stop')
