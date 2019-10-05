76a2763803f49f6187315c398f34aadf45aff865
daodeiv <daodeiv@Abv.bg>
2019-10-05 09:45:14 +0300

    3 body examples

--- daomechanics/generate_examples/3-body-great.py
+++ daomechanics/generate_examples/3-body-great.py
@@ -1,37 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 10 13:27:08 2019
-
-@author: David
-"""
-
-# -*- coding: utf-8 -*-
 """
 Created on Mon Jun 10 09:58:53 2019
 
 @author: David
 """
 
-from gravity import *
+from daomechanics.gravity import *
 import matplotlib.pyplot as plt
 from matplotlib import animation, rc
 from IPython.display import HTML
 import numpy as np
-print("gdasdasge")
-print("WOW WOW OWWO!!")
-step = 0.05
+
+step = 0.05 # integration step
 g = Ground()
 g.show_tragectory(True)
-#g.add_body(Body(1200, 5, 6,3*np.cos(np.pi / 4), -5*np.cos(np.pi / 4),h=step))
+
+
+### generate 3 body with different mass 
 g.add_body(Body(10000, -1,-15 ,-0.000001*np.cos(np.pi / 4), 0.0000001*np.cos(np.pi / 4),h=step))
 g.add_body(Body(1000, -6, -4,1*np.cos(np.pi / 4), 1*np.cos(np.pi / 4),h=step))
 g.add_body(Body(1000, -7, -23,1.2*np.cos(np.pi / 4), -1.5*np.cos(np.pi / 4),h=step))
-#g.add_body(Body(5000, 3, -9,2*np.cos(np.pi / 4),np.cos(np.pi / 4),h=step))
-#g.add_body(Body(200, 10, 10,-2*np.cos(np.pi / 4), -5*np.cos(np.pi / 4),h=step))
-#g.add_body(Body(2000, 12, 12, 0.0001,0.0001,h=step))
-#g.add_body(Body(5000, 3, 5, -1*np.cos(np.pi / 4), -1*np.cos(np.pi / 4),h=step))
-#g.add_body(Body(32, -5, -5, -3*np.cos(np.pi / 4), -1*np.cos(np.pi / 4),h=0.1))
-#g.add_body(Body(32, 3, 4,  -3*np.cos(np.pi / 4), -1*np.cos(np.pi / 4),h=0.1))
+
 g.calculate(r=100)
 
 fig = plt.figure(figsize=(6, 6))
@@ -39,13 +27,11 @@ fig = plt.figure(figsize=(6, 6))
   
 u = lambda t, x, y: 0
 v = lambda t, x, y: -10
-#
-# point.add_force(f)
-# z = point.calculate_radius_vector(20 * np.cos(np.pi / 4), +50 * np.sin(np.pi / 4), n=700)
-# plt.plot(z[:, 0], z[:, 1])
+
+
 n = 100
 size = int(g.get_size(n))
-# print(size)
+
 Writer = animation.writers['ffmpeg']
 writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 anim = animation.FuncAnimation(plt.gcf(), g.update_HTML_animation, interval=1, fargs=(fig,), frames=n, blit=False)
