07a4a6cfcc07d415f6f1fc9c91866a6468d4c7dd
daodavid93 <dstankov1993@gmail.com>
2020-04-10 12:38:20 +0300

    vercorized operations have been sucsesfuly integrated in gravity calculation

--- mechanic/gravity.py
+++ mechanic/gravity.py
@@ -1,52 +1,48 @@
+# most importent lib
 import numpy as np
+
+# visualizing
 import matplotlib.pyplot as plt
-import pandas as pd
 from matplotlib import animation, rc
 from IPython.display import HTML
-import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.axes3d as p3
+
+# representing data
+import pandas as pd
+# common python libs
 from datetime import datetime
 import warnings
-from random import *
+
 
 class Body:
     '''
-    fuc
+    just the class constructs the particle
     '''
 
-    ID = 0
-    @classmethod
-    def get_ID(cls):
-        '''
-        get unique id 
-        '''
-        Body.ID = Body.ID+1
-        return Body.ID
-
-    def __init__(self,  x0, y0, v_x, v_y, mass=1, h=0.001):
+    def __init__(self,  x0, y0, v_x, v_y, mass=1):
         '''
         Parameters
 
         '''
-        self.id = Body.get_ID()
-        # integration step
-        self.h = h
-
-        # ndarray current coors
+        # ndarray current coordinates
         self._mcoord = np.array([x0, y0])
-        # current velocity
+        # current current velocity
         self._mvelocity = np.array([v_x, v_y])
-
-        # all cordinates in time
-        self._coord = np.array(self._mcoord)
+        # mass
+        self._mass = mass
 
 
 class GravityField:
 
-    # g = 2.071×10−43 s2⋅m^−1⋅kg^−1.
-    # gravity constant
-    G = 0.001
+    '''
+      bla bla bla !!!!
 
-    def __init__(self, integratin_setp=0.1):
+    '''
+
+    def __init__(self, integratin_setp=0.1, G=0.01):
+        # gravity constant
+        self.g = G
 
         self.h = integratin_setp
         # curent coordinates and velocity of all bodies
@@ -55,7 +51,7 @@ class GravityField:
 
         # all  x cordinates for all  particule
         self.x_cordinates = np.array([])
-        # all y cordinates for all particlues
+        # all y cordinat for all particlues in all time  [ , ]
         self.y_cordinates = np.array([])
         self._masses = np.array([])
         self._force = np.array([])
@@ -70,65 +66,104 @@ class GravityField:
             self._mcoords = np.array([b._mcoord])
             self._mvelocity = np.array([b._mvelocity])
 
+        self._masses = np.append(self._masses, b._mass)
+
         if self.x_cordinates.size != 0:
             self.x_cordinates = np.append([self.x_cordinates], b._mcoord[0])
             self.y_cordinates = np.append([self.y_cordinates], b._mcoord[1])
         else:
             self.x_cordinates = np.array(b._mcoord[0])
             self.y_cordinates = np.array(b._mcoord[1])
+    
+    @classmethod
+    def calculate_gravity(cls, X_i, x_kj, M_i, M_kn, g =0.001, error_value=0.01):
+        ''''
+        G_ik = sum_i g*(M_i.M_kn)* (r_i - r_k)/( (r_i1 - r_k1)^2 +(r_i2 - r_k2)^2 )
+        G_i = S_i g.m.M(r_i - r_k)/(|r_i - r_k|^2)
 
-    def leaf_frog_step1(self):
+        The core of implementation.
+        This method calculates gravity force which acts on particle X_i.
+        The force G is a sum of all forces producing from every particle.
+
+        The method is written in NumPy vectorized in order to achieve the highest performance
+
+        Parameters :
+            X_i  : ndarray [ , ]
+            radius vector X_i = [x_ij]
+            example :
+             X_1 = [x_10,x_11] = [x(axis), y(axis)]
+
+            x_kj : ndarray
+            matrix of all coords exclude X_i
+            x_kj= [ [x_10, x_11 ], [..., ... ] , [x_k0,x_k1] ]
+
+            g : number 
+            gravity constant 
+
+            error_value : float
+            special setting  for fixing a problem related to ,
+            when |r| tends to zero (in real world this cannot happens)
+
+        Return :
+            Sum of all  gravity forces wich acts on particle X_i
+
+        '''
+
+        # dr matrix contain all delta elemes [[x_i0 - x_10, x_i1 - x_i1],[...,...],[x_i0 - x_k0, x_i1- x_k1]]
+        dr_kj = x_kj - X_i  #
+        mod_dr_kj = dr_kj**2
+        mod_dr_k = np.sum(mod_dr_kj, axis=1)
+
+        #!!! note since when |dr| --> 0 then F--> infinity
+        # and this is inposible for real world therefore ,to keep our model right we will handle that problem
+        if (mod_dr_k < error_value).any():
+            message = '|dr|, |dr|-->0 , , there for the dr has been repalced by configured error value {}  '.format(
+                error_value)
+            mod_dr_k[mod_dr_k < error_value] = error_value
+            warnings.showwarning(
+                message, filename='gravity.py', lineno=135, category=RuntimeWarning)
+
+        mod_dr_k = mod_dr_k.reshape(-1, 1)
+        M_kn = M_kn.reshape(-1, 1)
+
+        G = g*M_i*M_kn*(dr_kj/mod_dr_k)
+
+        sum_force_of_all_particles = np.sum(G, axis=0)
+
+        return sum_force_of_all_particles
+
+    def leapFrog_step1(self):
         """
-
-         step 1 of leapFrog integration
-         This method is used for calculating of x coordinates
-         for every particles before to be calculated the  new velocity values
+        leap frog step 1
+        x = x + v_1*self.h/2
         """
 
         #self.x1 = self.x1 + self.v1 * self.h / 2
         #self.x2 = self.x2 + self.v2 * self.h / 2
         self._mcoords = self._mcoords + self._mvelocity*(self.h/2)
+        #moment state
         x = self._mcoords[:, 0]
         y = self._mcoords[:, 1]
 
+        # track cordinate in time 
         if self.x_cordinates.shape == x.shape:
             self.x_cordinates = np.array([self.x_cordinates, x])
             self.y_cordinates = np.array([self.y_cordinates, y])
         else:
             self.x_cordinates = np.append(self.x_cordinates, [x], axis=0)
-            self.y_cordinates = np.append(self.y_cordinates, [y], axis=0)
+            self.y_cordinates = np.append(self.y_cordinates, [y], axis=0)    
 
-    def leap_frog_step2(self):
-        # v1 = Gravity.G*M * dx / ((dx ** 2 + dy ** 2) ** (3/2))
-        # v2 = Gravity.G*M * dy / ((dx ** 2 + dy ** 2) ** (3/2))
-
-        def calc_sum_all_forces(element, all_cordinates, masses):
-            # delta element_ij - all_corinates_kj
-            delta_r_M = all_cordinates-element
-            # all delta (x_ij - x_kj) ^2
-            # mutiple by mass (miss)
-            devider_modul_r = np.sum(delta_r_M**2, axis=1)**(3/2)
-            
-            if( (devider_modul_r==0).any() or  (devider_modul_r < 0.5 ).any() ):
-                warnings.warn("warning ".format(devider_modul_r))
-                
-
-            if( (devider_modul_r==0).any() or  (devider_modul_r < self.approx_error ).any() ):
-                warnings.warn("delta r < approx_error ,the force is skipp ".format(self.approx_error))
-                ##replaceee only this symbol
-                #devider_modul_r = 100000
-                devider_modul_r = 9000
-
-
-            g1 = 100*np.sum(delta_r_M[:, 0]*(1/devider_modul_r))
-            g2 = 100*np.sum(delta_r_M[:, 1]*(1/devider_modul_r))
-
-            return [g1, g2]
+    def leapFrog_step2(self):
+        '''
+        leapFrog algorithm step 2
+        v_{1/2} = v_1 + a(x_{1/2})*h
+        '''
 
         for i in range(self._mcoords.shape[0]):
             all_codinates = np.delete(self._mcoords, i, 0)
-            force = calc_sum_all_forces(
-                self._mcoords[i, :], all_codinates, self._masses)
+            masses = np.delete(self._masses, i)
+            force = GravityField.calculate_gravity(
+                self._mcoords[i, :], all_codinates, self._masses[i], masses,self.g, error_value=self.error)
 
             self._mvelocity[i][0] = self._mvelocity[i][0] + force[0]*self.h
             self._mvelocity[i][1] = self._mvelocity[i][1] + force[1]*self.h
@@ -139,27 +174,31 @@ class GravityField:
         self.Y_cordinates = pd.DataFrame(self.y_cordinates, columns=columns)
         print('calculation complete succsefuly')
 
-    def run(self, n, C = 0.1, number_frames=20, approx_error = 1):
+    def run(self, n, C=0.1, number_frames=20, approx_error=0.01):
         '''
         parametes :
         n- number of iteration
         '''
         start = datetime.now()
+        print('start:', str(start))
 
-        self.h = C 
-        print('start:' ,str(start))
+        self.h = C
         self.number_iter = n
-        self.number_frames =  number_frames
+        self.number_frames = number_frames
         self.frame_step = int(n/number_frames)
-        self.approx_error = approx_error 
+        self.approx_error = approx_error
         self.number_of_bodies = self._mvelocity.shape[0]
+        self.error = approx_error
+
+
         for i in range(n):
-            self.leaf_frog_step1()
-            self.leap_frog_step2()
+          self.leapFrog_step1()
+          self.leapFrog_step2() 
+          
+        
         self.save()
         end = datetime.now() - start
-        print('all cal process {}'.format(end) )
-
+        print('all cal process {}'.format(end))
 
     def result(self):
 
@@ -169,50 +208,38 @@ class GravityField:
         return ''
 
     def update_anim(self, i, arg):
-        plt.style.use('dark_background')
-    
         ax = plt.gca()
         arg.clf()
         k = self.frame_step*i
-        n_bodies =  self.number_of_bodies
+        n_bodies = self.number_of_bodies
         linewidth = 1/self.number_of_bodies
 
-        x, y = self.X_cordinates, self.Y_cordinates
+        #x, y = self.X_cordinates, self.Y_cordinates
+        X, Y = self.x_cordinates, self.y_cordinates
 
-        X,Y  = self.x_cordinates,self.y_cordinates
-
-        
+        #
         # for j in range(x.shape[1]):
-        '''
-        old imp. it's work
-        '''
-           
-            
         #     body = 'body_{}'.format(j)
+        #
+
         #     if(self.show_trajectory) :
         #         plt.plot(np.array(x[body]), np.array(y[body]),color='red',linewidth=linewidth)
         #     plt.scatter(x[body][k], y[body][k],color='white')
 
-        
-        x_plot,y_plot =X[k],Y[k]
-        plt.scatter(x_plot, y_plot ,color='skyblue')
-        plt.plot(x, y ,color='skyblue',linewidth=linewidth/2)    
-            
+        x_plot, y_plot = X[k], Y[k]
+        plt.scatter(x_plot, y_plot, color='skyblue', s=self._masses)
+        plt.plot(X, Y, color='skyblue', linewidth=linewidth)
+
         #q = plt.scatter(self.X_cordinates, self.Y_cordinates, color='indigo', linewidths=None)
-
-
-        ax.spines['left'].set_position('zero')
-        ax.spines['right'].set_color('none')
-        ax.spines['top'].set_color('none')
 
         #plt.scatter(0, 0, color='red', linewidths=0.0001)
         #plt.scatter(-10, 10, color='red', linewidths=0.0001)
         #plt.scatter(10, 10, color='red', linewidths=0.0001)
 
         # return particles
-    def save_animation(self, path=None ,show_trajectory=True):
-        self.show_trajectory =show_trajectory
+    def save_animation(self, path=None, show_trajectory=True):
 
+        self.show_trajectory = show_trajectory
         print('saving of animation')
         #fig = add_subplot(111, projection='3d')
         fig = plt.figure(figsize=(6, 6))
@@ -223,57 +250,25 @@ class GravityField:
 
         anim = animation.FuncAnimation(plt.gcf(), self.update_anim, interval=1, fargs=(
             fig,), frames=self.number_frames, blit=False)
-        name = str(np.random.randint(0, 1000))
         name = str(datetime.now())
         name = datetime.now().microsecond
         anim.save('resources/mp4/{}.mp4'.format(str(name)), writer=writer)
         HTML(anim.to_html5_video())
-\
 
+    def update3D(self, i, scatters):
+        pass
 
+    def save3D(self):
+        pass
 
 
 field = GravityField()
-# field.add_body(Body(3, 2, -0.3, -1))
+field.add_body(Body(1, 1, 1, 1, mass=2))
+#field.add_body(Body(1.00001, 1.00001,1 ,1,mass=2))
 
-# field.add_body(Body(-15 ,5,-0.00001*np.cos(np.pi / 4), 0.0001*np.cos(np.pi / 4)))
-# #field.add_body(Body( 6, -4,1*np.cos(np.pi / 4), 1*np.cos(np.pi / 4)))
-# #field.add_body(Body(1, 4,1*np.cos(np.pi / 4), 1*np.cos(np.pi / 4)))
-# #field.add_body(Body( 5, 1, 1*np.cos(np.pi / 4), -1*np.cos(np.pi / 4)))
-
+field.add_body(Body(2, 2, 2, 2, mass=3))
+field.add_body(Body(3, 3, 3, 3, mass=1))
 
 
-
-
-
-for i in range(2):
-    v1 = uniform(0,360)
-    v2 = uniform(0,360)
-    x1 = uniform(-50,50)
-    x2 =uniform(-10,15)
-    m = uniform(1000,10000)
-    field.add_body(Body( x1,x2 , np.cos(np.pi*(v1/180))/10000, 0.001*np.cos(np.pi*(v1/180))/10000 ))
-
-field.add_body(Body(1, 4,1*np.cos(np.pi / 4), 1*np.cos(np.pi / 4)))
-field.add_body(Body( 5, 1, 1*np.cos(np.pi / 4), -1*np.cos(np.pi / 4)))    
-
-field.run(1000, C = 0.001, number_frames=50,approx_error=2.5)
+field.run(5000, C=0.001, number_frames=30, approx_error=0.01)
 field.save_animation()
-
-#field.add_body(Body(1, 1, 0.2, 0.2))
-#
-print('З')
-#x, y = field.result()
-
-# plt.plot(x['body_1'][0:50])
-
-# plt.plot(x['body_0'][0:50])
-# # print(x['body_0'])
-# plt.show()
-# plt.plot(np.log(field._force[1:20]))
-# plt.plot(field._force[1:20])
-# plt.show()
-
-
-# def update_HTML_animation(X,Y):
-
