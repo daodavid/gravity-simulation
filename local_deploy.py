5eda2578c24b611985dcdee80bb0e10f08ef9e52
daodavid93 <dstankov1993@gmail.com>
2020-04-22 17:32:25 +0300

    update

---
+++ local_deploy.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='dao-gravity-simulation',
+version='0.1',
+description='installing gravity simulation',
+url='https://github.com/daodavid/gravity-simulation',
+author='daodeiv',
+author_email='dstankov1993@gmail.com',
+license='dadeiv-gravity',
+packages=['gravity_simulation'],
+zip_safe=True)
+
