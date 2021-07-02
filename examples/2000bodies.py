0667ba5ff20f87c8ecb6a382ef363fe7f465995b
Glubshev Artem <36304382+SomeAnonimCoder@users.noreply.github.com>
2021-07-02 07:10:06 -0400

    Fix unrecognized argument error
    This fixes #17 issue

--- examples/2000bodies.py
+++ examples/2000bodies.py
@@ -4,7 +4,7 @@ from gravity_simulation.gravity import Body
 field = GravityField()
 # v = (0,10)
 # print(v[1])
-field.generate_random(3500,mass=[100,500],r = [-40,40])
+field.generate_random(3500,mass=[100,500], r_x=[-40,40], r_y=[-40,40])
 
 
 field.add_body(Body(x0=22,y0=22,v_x=0,v_y=0,mass = 15000))
